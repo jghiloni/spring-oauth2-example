a745686a01e5113aa7081884068ec317eaad9937
Josh Ghiloni <ghiloni@gmail.com>
2014-11-25 15:51:09 -0700

    fix config

--- src/main/java/com/ecsteam/sample/oauth2/configuration/SecurityConfig.java
+++ src/main/java/com/ecsteam/sample/oauth2/configuration/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.ecsteam.sample.oauth2.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	@Value("${demoapp.loginUrl}")
+	private String loginUrl;
+	
 //	@Override
 //	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.inMemoryAuthentication().withUser("josh").password("Passw0rd").roles("USER", "IMPERSONATOR").and()
@@ -20,6 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	public UserDetailsService userDetailsServiceBean() throws Exception {
 //		return super.userDetailsServiceBean();
 //	}
+	
+	@Bean
+	public LoginUrlAuthenticationEntryPoint authenticationEntryPoint() {
+		return new LoginUrlAuthenticationEntryPoint(loginUrl);				
+	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
