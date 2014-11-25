735175dbdf879e69c6c024467e27121d2414ab7f
Josh Ghiloni <ghiloni@gmail.com>
2014-11-25 15:56:47 -0700

    this time with feeling

--- src/main/java/com/ecsteam/sample/oauth2/rest/BaseService.java
+++ src/main/java/com/ecsteam/sample/oauth2/rest/BaseService.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class BaseService {
 
 	private SubServiceController controller;
